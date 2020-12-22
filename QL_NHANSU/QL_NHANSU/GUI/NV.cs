fd35b717c79d8c5f7cc113b9811b3289de5484c3
DuyMTA <59283692+DuyMTA@users.noreply.github.com>
2020-12-22 08:49:27 +0700

    duy return
    file

---
+++ QL_NHANSU/QL_NHANSU/GUI/NV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QL_NHANSU.GUI
+{
+    public partial class NV : UserControl
+    {
+        public NV()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
